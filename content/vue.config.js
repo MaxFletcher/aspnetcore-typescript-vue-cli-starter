28eb3e434bfdf0434cae412e2e871c5cc0aa6256
Max <=>
2018-09-21 20:26:12 +0100

    Initial Project Structure.

---
+++ content/vue.config.js
@@ -0,0 +1,15 @@
+﻿const path = require('path');
+
+module.exports = {
+    outputDir: 'wwwroot/dist',
+    filenameHashing: false,
+    chainWebpack: config => {
+        config.plugins.delete('html');
+        config.plugins.delete('preload');
+        config.plugins.delete('prefetch');
+        config.plugins.delete('hmr');
+        config.entry('app').delete('./src/main.ts');
+        config.entry('app').add('./ClientApp/main.ts');
+        config.resolve.alias.set('@', path.resolve(__dirname, 'ClientApp'));
+    }
+};
