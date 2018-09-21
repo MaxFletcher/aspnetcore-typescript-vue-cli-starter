7f59ecd30b9437dfb50fd65cd1f912d66d8970e2
Max <=>
2018-09-21 20:40:30 +0100

    Added counter example to about view.

--- content/ClientApp/views/About.vue
+++ content/ClientApp/views/About.vue
@@ -1,5 +1,22 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <p>Count: {{counter}}</p>
+    <button @click="increment()">Increment count</button>
   </div>
 </template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class About extends Vue {
+  // data
+  private counter: number = 0;
+
+  // methods
+  private increment(): void {
+      this.counter++;
+  }
+}
+</script>
